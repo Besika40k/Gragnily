793190ac45970453c454ce28d56b3ab73a611175
George <109150570+Nok1on1@users.noreply.github.com>
2025-04-23 15:09:52 +0400

    Refactor duplicate user and email check: unify error messages for better clarity

--- backend/middleware/verifySignUp.js
+++ backend/middleware/verifySignUp.js
@@ -11,17 +11,18 @@ checkDuplicate = asyncHandler(async (req, res, next) => {
       email: req.body.email,
     });
 
+    if (inUseUser && inUseEmail) {
+      res.status(500).json({ message: `IN_USE_USER, IN_USE_EMAIL` });
+      return;
+    }
+
     if (inUseUser) {
-      res
-        .status(500)
-        .json({ message: `User ${req.body.username} is already in use!` });
+      res.status(500).json({ message: `IN_USE_USER` });
       return;
     }
 
     if (inUseEmail) {
-      res
-        .status(500)
-        .json({ message: `Email ${req.body.email} is already in use!` });
+      res.status(500).json({ message: `IN_USE_EMAIL` });
       return;
     }
     next();
