cdca6e6a57bdb7fe4344867d34328e8a008f2325
ბესო <besomeskhia40k@gmail.com>
2025-04-29 13:27:09 +0400

    frontend v1.2
    1. Added fully functional individual book pages
    2. edited so that search redirects to such book pages on click

--- frontend/src/Pages/BookPage.jsx
+++ frontend/src/Pages/BookPage.jsx
@@ -4,22 +4,35 @@ import { useEffect, useState } from "react";
 const BookPage = () => {
   const { id } = useParams(); // Get the dynamic ID
   const [book, setBook] = useState(null);
-
+  const cleanId = id.substring(1, id.length); // Clean the ID to ensure it's a number
   useEffect(() => {
-    // Fetch book data (simulate or from API)
-    fetch(`https://your-api.com/books/${id}`)
-      .then((res) => res.json())
-      .then((data) => setBook(data));
-  }, [id]);
-
+    fetch(`https://gragnily.onrender.com/api/books/${cleanId}`, {
+      method: "GET",
+      credentials: "include",
+    })
+      .then(async (response) => {
+        const data = await response.json();
+        console.log(response, data);
+        if (response.ok) {
+          console.log("Book retriaval successful!", data);
+          setBook(data);
+        } else {
+          switch (data.message) {
+          }
+        }
+      })
+      .catch((error) => {
+        console.error("Network error:", error);
+        alert("Something went wrong. Please try again later.");
+      });
+  }, [cleanId]);
   if (!book) return <div>Loading...</div>;
+  console.log("book data", book._id);
 
   return (
     <div>
       <h1>{book.title}</h1>
-      <p>Author: {book.author}</p>
-      <p>Description: {book.description}</p>
-      {/* Add image, price, reviews, etc. */}
+      <img src={book.cover_image_url} alt="" />
     </div>
   );
 };
