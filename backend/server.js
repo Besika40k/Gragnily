8c15beb9ccd5db768a390a458a46e5f48a7b3b16
George <109150570+Nok1on1@users.noreply.github.com>
2025-05-17 18:53:51 +0400

    Add logging for NODE_ENV in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 connectDb();
 
+console.log(process.env.NODE_ENV);
+
 const port = process.env.PORT || 5000;
 
 app.use(cookieParser());
