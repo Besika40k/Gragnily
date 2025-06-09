0e1cc41ffd487088a3c46091606d475369b99369
George <109150570+Nok1on1@users.noreply.github.com>
2025-06-09 16:29:16 +0400

    .

---
+++ frontend/src/Pages/Bookmarked.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Bookmarked = () => {
+  return (
+    <div>
+        <Link to="/books">bookmarked</Link>
+    </div>
+  );
+};
+
+export default Bookmarked;
